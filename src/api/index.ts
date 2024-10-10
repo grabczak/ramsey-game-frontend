7cd92162d0352d4e433ed76435b8e6bce75f3f84
Kacper Grabczak <kacpergrabczak@gmail.com>
2024-10-10 21:42:47 +0200

    Install axios

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const URL = {
+  DEV: "http://127.0.0.1:5000",
+  PROD: "https://ramsey-game-backend-h4ewacgsh9frd9f9.polandcentral-01.azurewebsites.net",
+};
+
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === "development" ? URL.DEV : URL.PROD,
+});
+
+export const play = () => {
+  return api.get("/");
+};
