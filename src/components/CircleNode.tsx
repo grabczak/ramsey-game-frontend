b55249ef84373cd94044de8f63a75839434f9e43
Kacper Grabczak <kacpergrabczak@gmail.com>
2024-11-02 10:19:45 +0100

    Refactor circle node

--- src/components/CircleNode.tsx
+++ src/components/CircleNode.tsx
@@ -1,21 +1,15 @@
 import { Handle, Position } from "@xyflow/react";
 
-export function CircleNode({ data }: { data: { label: string } }) {
+type TCircleNodeProps = {
+  data: {
+    label: string;
+  };
+};
+
+export function CircleNode({ data }: TCircleNodeProps) {
   return (
     <>
-      <div
-        style={{
-          width: "15px",
-          height: "15px",
-          border: "1px solid rgb(26, 25, 43)",
-          borderRadius: "50%",
-          background: "rgb(255, 255, 255",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          fontSize: 8,
-        }}
-      >
+      <div className="w-3 h-3 border border-solid border-black rounded-full bg-white flex justify-center items-center text-[8px] font-mono">
         <Handle type="source" position={Position.Top} />
         <div>{data.label}</div>
         <Handle type="target" position={Position.Top} />
